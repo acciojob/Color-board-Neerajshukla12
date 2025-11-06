433300cf8d94678c6cd13af93653f3bb23c14687
Neeraj Kumar Shukla <132157056+Neerajshukla12@users.noreply.github.com>
2025-11-06 10:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const container = document.querySelector('.container');
 
 const boxCount = 800;
 
-for(let i = 0; i < boxCount; i++){
+for (let i = 0; i < boxCount; i++) {
     const box = document.createElement('div');
     box.classList.add('square');
 
     box.addEventListener('mouseover', () => {
         box.style.background = getRandomColor();
         setTimeout(() => {
-            box.style.background = "rgb(29, 29, 29)"; // return to default
+            box.style.background = "rgb(29, 29, 29)";
         }, 1000);
     });
 
     container.appendChild(box);
 }
 
-function getRandomColor(){
+function getRandomColor() {
     const colors = [
         '#e74c3c', '#8e44ad', '#3498db', '#e67e22',
         '#2ecc71', '#1abc9c', '#f1c40f', '#d35400'
